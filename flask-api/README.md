a463d35716cf0c2cc5246568a6ecac68786b1e90
Sam Xu <samthebosssss@gmail.com>
2020-09-29 13:31:42 +0930

    beter project structure

--- flask-api/README.md
+++ flask-api/README.md
@@ -8,4 +8,13 @@ To run this application, first install all required dependencies:
 
 You can also setup a venv if you want to have clean, project specific dependencies.
 
-Once everything is installed, run `python3 main.py`.
+Once everything is installed, run `. ./setup.sh`. (Note: call setup.sh again if you ever clear your environmental vars, such as restarting your pc).
+
+## Post config
+After running the "setup.sh", you can:
+
+* Use `flask db-init` to run the schema.sql file.
+* Use `flask run` to start the flask app.
+
+All setup/config are based on the official flask documentation, see more here: https://flask.palletsprojects.com/en/1.1.x/tutorial/layout/
+Fun DB related reading material (injection free): https://flask.palletsprojects.com/en/1.1.x/patterns/sqlite3/
---
+++ flask-api/flaskr/__init__.py
@@ -0,0 +1,46 @@
+import os
+# add more blue prints here. "project" is the directory, "test_page" is the file name, "simple_page" is the blueprint object.
+
+from flask import Flask
+from . import test_page
+
+
+# this file is the main dirver for flask.
+def create_app(test_config=None):
+        
+    # create the application
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+    app.config["DEBUG"] = True
+    app.register_blueprint(test_page.simple_page)
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+
+    # app route defines the base methods. @ annotation is used to mark routes on functions
+    @app.route('/', methods=['GET'])
+    def home():
+        return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+
+    # start the DB module, enables `flask init-db`. See db.py for implementation
+    from . import db
+    db.init_app(app)
+
+
+    # start the application
+    return app
