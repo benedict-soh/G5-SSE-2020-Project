21954d64c3411bb06d0703f1012676a1d9a5ecfd
a1719756 <johnnobullas@hotmail.com>
2020-10-10 19:59:18 +1030

    Test page for voting

---
+++ frontend/src/test/index.js
@@ -0,0 +1,15 @@
+const TestIndex = () =>{
+
+	return(
+		<Layout title="Vote" >
+			<h2>Voting Page</h2>
+			<div className="flex items-center justify-center m-2">
+				<Link href="/test/submit">
+					<a href="/test/submit">Submit Vote</a>
+				</Link>
+			</div>
+		</Layout>
+		)
+}
+
+export default TestIndex;
