93d8bba68376090db35a760766ecdadfc8296262
a1719756 <johnnobullas@hotmail.com>
2020-10-22 13:42:03 +1030

    Voting event CRUD migrated to material UI

---
+++ frontend/src/components/voting_events_rows.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import { FormControl } from '@material-ui/core';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 16,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
+
+export default function VotingEventRows({voting_events }){
+  var date;
+  var day;
+  var mon;
+  var year;
+  var vstart;
+  var vend;
+  return (
+    <TableBody>
+    {voting_events.map((row) => {
+      date = new Date(row.vote_start);
+      day = date.getDate();
+      mon = date.getMonth() + 1;
+      year = date.getFullYear();
+      vstart = day + "/" + mon + "/" + year;
+      date = new Date(row.vote_end);
+      day = date.getDate();
+      mon = date.getMonth() + 1;
+      year = date.getFullYear();
+      vend = day + "/" + mon + "/" + year;
+      return (
+      <StyledTableRow key={row.event_name}>
+        <StyledTableCell component="th" scope="row">
+          {row.event_name}
+        </StyledTableCell>
+        <StyledTableCell align="right">{row.year}</StyledTableCell>
+        <StyledTableCell align="right">{vstart}</StyledTableCell>
+        <StyledTableCell align="right">{vend}</StyledTableCell>
+        <StyledTableCell align="right">
+          <Link to={"/voting_events/update/"+row.id}>
+            <Button variant="contained" color="primary">
+              Edit
+            </Button>
+          </Link>
+        </StyledTableCell>
+        <StyledTableCell align="right">
+          <Button variant="contained"
+            color="secondary"
+            onClick={async() => {
+              const response = await fetch("/voting-events/"+row.id+"/delete", {
+          			method: "DELETE"
+          		});
+          		if(response.ok) {
+          			console.log("Deleted event");
+          			window.location.replace("/voting_events");
+          		} else {
+          			console.log("Didnt delete event");
+          		}
+            }}>
+              Delete
+          </Button>
+        </StyledTableCell>
+      </StyledTableRow>
+      )
+    })}
+    </TableBody>
+  )
+};
--- frontend/src/parties/partyForm.js
+++ frontend/src/parties/partyForm.js
@@ -141,3 +141,45 @@ export default function VotingEventForm({voteEvent, event_id}) {
 		</form>
 		)
 }
+
+
+// <form>
+// 			<h2>Voting Event {CRUD} Page</h2>
+// 			<label for="event_name">Event name:</label>
+// 			<input
+// 				id="event_name"
+// 				type="text"
+// 				value={event_name}
+// 				onChange={(event) => setEventName(event.target.value)}
+// 				placeholder="Event Name"
+// 				required />
+// 			<label for="year">Event Year:</label>
+// 			<input
+// 				id="year"
+// 				type="number"
+// 				value={year}
+// 				onChange={(event) => setEventYear(event.target.value)}
+// 				placeholder="2020"
+// 				required />
+// 			<label for="vote_start">Vote Start Date:</label>
+// 			<input
+// 				id="vote_start"
+// 				type="date"
+// 				value={vote_start}
+// 				onChange={(event) => setVoteStart(event.target.value)}
+// 				required />
+// 			<label for="vote_end">Vote End Date:</label>
+// 			<input
+// 				id="vote_end"
+// 				type="date"
+// 				value={vote_end}
+// 				onChange={(event) => setVoteEnd(event.target.value)}
+// 				required />
+// 			<button type="button" onClick={ e => {
+// 				voteEvent ?
+// 					updateEvent()
+// 				:
+// 					createEvent()}
+// 			}>
+// 			Submit Event</button>
+// </form>
